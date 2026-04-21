cc51ddc8f0fd0140ca551b7880d2616ebfd5ccd8
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-21 15:12:22 +0530

    Let's developer a complex Map with custom Annotation in SwiftUI - Part 1

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    //MARK: - PROPERTIES
+    
+    @State private var region: MKCoordinateRegion = {
+       
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.437759)
+        var mapZoomLavel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLavel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    //MARK: - BODY
     var body: some View {
-        Text("MapView")
+        Map(initialPosition: .region(region)) {
+            // No overlays or annotations yet
+            ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.location) {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                    }
+                }
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     MapView()
 }
---
+++ Africa/Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Ajeet Sharma on 21/04/26.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        .init(latitude: latitude, longitude: longitude)
+    }
+}
