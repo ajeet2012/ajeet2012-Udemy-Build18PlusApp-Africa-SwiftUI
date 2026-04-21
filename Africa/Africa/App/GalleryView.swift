f752090329ee21636870b192c9faf3a2f863633f
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-21 18:25:17 +0530

    Let's create a photo gallery with grid layout using Swift UI

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -9,19 +9,81 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: - PROPERTIES
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    @State private var selectedAnimal: String = "lion"
+    @State private var isImageSelected: Bool = false
+    
+    //SIMPLE GRID DEFINITION
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    //EFFICIENT GRID DEFINITION
+   // let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible(minimum: 100)), count: 3)
+    
+    //DYNAMIC GRID LAYOUT
+    
+    @State private var gridLayout: [GridItem] = Array(repeating: .init(.flexible()), count: Int(3.0))
+    @State private var grideColumn: Double = 3.0
+    
+    //MARK: -  FUNCTION
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(grideColumn))
+    }
     
     //MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery View")
+            
+            VStack(alignment: .center, spacing: 30) {
+                //MARK: - SELECTED IMAGE
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                // MARK: -  SLIDER
+                
+                Slider(value: $grideColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: grideColumn) { oldValue, newValue in
+                        gridSwitch()
+                    }
+                
+                // MARK: -  GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                self.selectedAnimal = item.image
+                                isImageSelected.toggle()
+                            }
+                            .sensoryFeedback(.impact, trigger: isImageSelected)
+                    }//: Loop
+                }//: LazyVGrid
+                .animation(.easeIn, value: gridLayout)
+                .onAppear (perform: {
+                    gridSwitch()
+                })
+            } //: VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 20)
         }//: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
-        
-        
     }
 }
 
 #Preview {
     GalleryView()
 }
+
